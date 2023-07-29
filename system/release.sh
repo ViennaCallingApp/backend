14775aa8a06c6459c832cd74ae693b54e79e736c
Jan Jaworski <bleggbeard@protonmail.com>
2023-07-29 22:51:05 +0200

    feat: Add deployment

---
+++ system/release.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+set -euo pipefail
+
+IMAGE_TAG=$1
+docker buildx build --push \
+  --tag jajcoszek/viennacalling-backend:${IMAGE_TAG} \
+  --tag jajcoszek/viennacalling-backend:latest \
+  --platform linux/amd64,linux/arm64 \
+  .
+
+cd deploy
+../kustomize edit set image jajcoszek/viennacalling-backend=jajcoszek/viennacalling-backend:${IMAGE_TAG}
