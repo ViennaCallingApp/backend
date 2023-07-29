4066b93b92b6d6de9a527f3542867d6b38d0e86d
KasnocknDave <dhn@xxxlgroup.com>
2023-07-30 00:00:22 +0200

    feat: Add tests

---
+++ src/test/java/com/ViennaCallingApp/backend/ViennaCallingAppBackendTests.java
@@ -0,0 +1,13 @@
+package com.ViennaCallingApp.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ViennaCallingAppBackendTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
