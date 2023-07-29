0463230b6a6d66f6c67641c1bfd176b2958a01c0
KasnocknDave <dhn@xxxlgroup.com>
2023-07-29 18:04:41 +0200

    feat: Finish POPTIS scraper

--- src/main/java/com/ViennaCallingApp/backend/model/Box.java
+++ src/main/java/com/ViennaCallingApp/backend/model/Box.java
@@ -1,26 +1,26 @@
 package com.ViennaCallingApp.backend.model;
 
+import org.jsoup.nodes.Node;
+import org.jsoup.nodes.TextNode;
+
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Box {
 
-    private String title;
-    private String content;
+    private List<String> contents;
 
-    public Box(){
-    }
+    public Box(Elements elements){
+        contents = new ArrayList<>();
+        List<Node> nodes = elements.first().childNodes();
 
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getContent() {
-        return content;
-    }
-
-    public void setContent(String content) {
-        this.content = content;
+        for ( Node node : nodes ){
+            if(node instanceof TextNode && !node.toString().equals("")){
+               contents.add(node.toString());
+            }
+        }
     }
 }
